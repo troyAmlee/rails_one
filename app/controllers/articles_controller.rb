e598c077b1812347e2be0569dfa8ca944d05f9f2
troyAmlee <troyamlee19@gmail.com>
2020-03-17 19:57:23 -0500

    Added routes/navbar, ability to edit + create + view all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,26 @@ class ArticlesController < ApplicationController
   end
 
   def index
-
+    @article = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully edited"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
